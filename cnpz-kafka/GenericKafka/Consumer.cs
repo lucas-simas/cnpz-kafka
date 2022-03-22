6158c3175635c5c8eaf33742447e43abd8ac03ef
Lucas Simas <lucasrj03@gmail.com>
2022-03-21 23:47:43 -0300

    Producer genérico preparado para uso

---
+++ cnpz-kafka/GenericKafka/Consumer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cnpz_kafka.GenericKafka
+{
+	internal class Consumer
+	{
+	}
+}
---
+++ cnpz-kafka/GenericKafka/Producer.cs
@@ -0,0 +1,34 @@
+﻿using Confluent.Kafka;
+
+namespace cnpz_kafka.GenericKafka
+{
+    /// <summary>
+    /// Classe responsável para chamadas aos producers (enviar mensagem) do KAFKA
+    /// </summary>
+    public class Producer
+    {
+        /// <summary>
+        /// Task para envio de mensagem ao KAFKA
+        /// </summary>
+        /// <param name="topic">Tópico da mensagem kafka</param>
+        /// <param name="message">Mensagem que será enviada</param>
+        /// <param name="ip_port">IP e Porta para endereço da mensagem + topico</param>
+        public async Task SendKafka( string topic, string message,  string ip_port = "127.0.0.1:9094" )
+        {
+            var config = new ProducerConfig { BootstrapServers = ip_port };
+
+            using var p = new ProducerBuilder<Null, string>(config).Build();
+
+            {
+                try
+                {
+                    var dr = await p.ProduceAsync(topic, new Message<Null, string> { Value = message });
+                }
+                catch (ProduceException<Null, string> e)
+                {
+                    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                }
+            }
+        }
+    }
+}
