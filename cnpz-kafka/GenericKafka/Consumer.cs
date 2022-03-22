8167687294e7bb24b3128652c535d7b558b89fce
Lucas Simas <lucasrj03@gmail.com>
2022-03-22 00:04:40 -0300

    Melhorias para consumer e producer genéricos

--- cnpz-kafka/GenericKafka/Consumer.cs
+++ cnpz-kafka/GenericKafka/Consumer.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Confluent.Kafka;
 
 namespace cnpz_kafka.GenericKafka
 {
-	internal class Consumer
-	{
-	}
+    /// <summary>
+    /// Classe responsável para chamadas aos consumers (leitura mensagem) do KAFKA
+    /// </summary>
+    public class Consumer
+    {
+        /// <summary>
+        /// Task para leitura de mensagem no KAFKA
+        /// </summary>
+        /// <param name="topic">Tópico da mensagem kafka</param>
+        /// <param name="ip_port">IP e Porta para endereço da mensagem + topico</param>
+        /// <param name="type">Tipo de leitura ao resetar ponteiro</param>
+        static public string ReadKafka(string topic, string ip_port = "127.0.0.1:9094", string type = "latest") {   
+
+            var conf = new ConsumerConfig {
+                GroupId = topic,
+                BootstrapServers = ip_port,
+                AutoOffsetReset = type == "latest" ? AutoOffsetReset.Latest : AutoOffsetReset.Earliest
+            };
+            
+            //Consumer
+            using var c = new ConsumerBuilder<Ignore, string>(conf).Build();
+            
+            //Token de cancelamento
+            var cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) => {
+                e.Cancel = true;
+                cts.Cancel();
+            };
+
+            var cr = c.Consume(cts.Token);
+            c.Close();
+
+            return cr.Message.Value ;
+
+        }
+    }
 }
--- cnpz-kafka/GenericKafka/Producer.cs
+++ cnpz-kafka/GenericKafka/Producer.cs
@@ -13,22 +13,21 @@ namespace cnpz_kafka.GenericKafka
         /// <param name="topic">Tópico da mensagem kafka</param>
         /// <param name="message">Mensagem que será enviada</param>
         /// <param name="ip_port">IP e Porta para endereço da mensagem + topico</param>
-        public async Task SendKafka( string topic, string message,  string ip_port = "127.0.0.1:9094" )
-        {
-            var config = new ProducerConfig { BootstrapServers = ip_port };
+        static public Boolean SendKafka( string topic, string message,  string ip_port = "127.0.0.1:9094" ) {
 
+            var config = new ProducerConfig { BootstrapServers = ip_port };
             using var p = new ProducerBuilder<Null, string>(config).Build();
 
-            {
-                try
-                {
-                    var dr = await p.ProduceAsync(topic, new Message<Null, string> { Value = message });
-                }
-                catch (ProduceException<Null, string> e)
-                {
-                    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
-                }
+            try {
+                p.Produce(topic, new Message<Null, string> { Value = message });
             }
+            catch (ProduceException<Null, string> e) {
+                Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                return false;
+            }
+
+            return true;
+
         }
     }
 }
